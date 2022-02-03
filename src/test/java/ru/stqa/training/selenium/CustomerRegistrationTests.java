cffd9eb50aa3cd9faa0a48e3be587ade27e1c964
Даниил Моисеев <damoiseev@alfabank.ru>
2022-02-03 13:54:50 +0300

    Реализованы проверки

--- src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
+++ src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
@@ -6,11 +6,30 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.Set;
+
+import static java.util.stream.Collectors.toSet;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CustomerRegistrationTests extends TestBase {
 
     @ParameterizedTest
     @MethodSource("ru.stqa.training.selenium.DataProvider#validCustomers")
     public void canRegisterCustomer(Customer customer) {
+        driver.get("http://localhost/litecart/admin");
+        if (driver.findElements(By.id("box-login")).size() > 0) {
+            driver.findElement(By.name("username")).sendKeys("admin");
+            driver.findElement(By.name("password")).sendKeys("admin");
+            driver.findElement(By.name("login")).click();
+            wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
+        }
+
+        driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
+        Set<String> oldIds = driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
+                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
+                .collect(toSet());
+
         driver.get("http://localhost/litecart/en/create_account");
         driver.findElement(By.name("firstname")).sendKeys(customer.getFirstname());
         driver.findElement(By.name("lastname")).sendKeys(customer.getLastname());
@@ -30,6 +49,14 @@ public class CustomerRegistrationTests extends TestBase {
         driver.findElement(By.name("password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("confirmed_password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("create_account")).click();
+
+        driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
+        Set<String> newIds = driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
+                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
+                .collect(toSet());
+
+        assertTrue(newIds.containsAll(oldIds));
+        assertEquals(newIds.size(), oldIds.size() + 1);
     }
 
 }
