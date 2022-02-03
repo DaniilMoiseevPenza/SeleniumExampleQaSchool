78fac83cfd571f61dbd2bbeabc8edb91f9dcaba4
Даниил Моисеев <damoiseev@alfabank.ru>
2022-02-03 13:51:52 +0300

    Генерация данных отоделена от тестов

--- src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
+++ src/test/java/ru/stqa/training/selenium/CustomerRegistrationTests.java
@@ -1,31 +1,34 @@
 package ru.stqa.training.selenium;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class CustomerRegistrationTests extends TestBase {
 
-    @Test
-    public void canRegisterCustomer() {
-        String email = "adam"+System.currentTimeMillis()+"@smith.me";
-        driver.get("https://www.litecart.net/en/account/create");
-        driver.findElement(By.name("firstname")).sendKeys("Adam");
-        driver.findElement(By.name("lastname")).sendKeys("Smith");
-        driver.findElement(By.name("address1")).sendKeys("Hidden Place");
-        driver.findElement(By.name("postcode")).sendKeys("12345");
-        driver.findElement(By.name("city")).sendKeys("New City");
+    @ParameterizedTest
+    @MethodSource("ru.stqa.training.selenium.DataProvider#validCustomers")
+    public void canRegisterCustomer(Customer customer) {
+        driver.get("http://localhost/litecart/en/create_account");
+        driver.findElement(By.name("firstname")).sendKeys(customer.getFirstname());
+        driver.findElement(By.name("lastname")).sendKeys(customer.getLastname());
+        driver.findElement(By.name("address1")).sendKeys(customer.getAddress());
+        driver.findElement(By.name("postcode")).sendKeys(customer.getPostcode());
+        driver.findElement(By.name("city")).sendKeys(customer.getCity());
 
         driver.findElement(By.cssSelector("[id ^= select2-country_code]")).click();
-        driver.findElement(By.cssSelector(".select2-results__option[id $= US]")).click();
-        wait.until((WebDriver d) -> d.findElement(By.cssSelector("select[name=zone_code] option[value=KS]")));
-        new Select(driver.findElement(By.name("zone_code"))).selectByValue("KS");
+        driver.findElement(By.cssSelector(
+                String.format(".select2-results__option[id $= %s", customer.getCountry()))).click();
+        wait.until((WebDriver d) -> d.findElement(
+                By.cssSelector(String.format("select[name=zone_code] option[value=%s]", customer.getZone()))));
+        new Select(driver.findElement(By.name("zone_code"))).selectByValue(customer.getZone());
 
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("phone")).sendKeys("+0123456789");
-        driver.findElement(By.name("password")).sendKeys("qwerty");
-        driver.findElement(By.name("confirmed_password")).sendKeys("qwerty");
+        driver.findElement(By.name("email")).sendKeys(customer.getEmail());
+        driver.findElement(By.name("phone")).sendKeys(customer.getPhone());
+        driver.findElement(By.name("password")).sendKeys(customer.getPassword());
+        driver.findElement(By.name("confirmed_password")).sendKeys(customer.getPassword());
         driver.findElement(By.name("create_account")).click();
     }
 
---
+++ src/test/java/ru/stqa/training/selenium/DataProvider.java
@@ -0,0 +1,15 @@
+package ru.stqa.training.selenium;
+
+public class DataProvider {
+    public static Object[][] validCustomers() {
+        return new Object[][] {
+                { Customer.newEntity()
+                        .withFirstname("Adam").withLastname("Smith").withPhone("+0123456789")
+                        .withAddress("Hidden Place").withPostcode("12345").withCity("New City")
+                        .withCountry("US").withZone("KS")
+                        .withEmail("adam"+System.currentTimeMillis()+"@smith.me")
+                        .withPassword("qwerty").build() },
+                /* ... */
+        };
+    }
+}
